35d6f72b7957c686919aceace24b6b055dd996d3
Alex McKenzie <alex.thewlis.mckenzie@gmail.com>
2022-02-13 19:22:32 +0100

    Install conda kernel in setup script

--- setup-pace.sh
+++ setup-pace.sh
@@ -3,3 +3,4 @@
 conda env create -f env_gpu.yml
 pip install git+https://github.com/ulissigroup/amptorch.git@MCSH_paper1_lmdb
 wget https://github.com/medford-group/bdqm-vip/raw/master/data/amptorch_data/oc20_3k_train.traj
+conda run -n bdqm-hpopt python -m ipykernel install
