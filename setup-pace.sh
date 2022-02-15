312dd6aa91e1a36c4498bc71fa63d59e327d623a
Alex McKenzie <alex.thewlis.mckenzie@gmail.com>
2022-02-15 12:23:17 +0100

    Add .traj data files

--- setup-pace.sh
+++ setup-pace.sh
@@ -2,4 +2,3 @@
 
 conda env create -f env_gpu.yml
 pip install git+https://github.com/ulissigroup/amptorch.git@MCSH_paper1_lmdb
-wget https://github.com/medford-group/bdqm-vip/raw/master/data/amptorch_data/oc20_3k_train.traj
