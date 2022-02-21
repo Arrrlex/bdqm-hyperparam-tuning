ed7364c1c7810ea79a6ca7bbe9ac9a271d9f1d93
Alex McKenzie <alex.thewlis.mckenzie@gmail.com>
2022-02-21 11:11:31 +0100

    Fix line endings

--- hpopt/create_lmdb.py
+++ hpopt/create_lmdb.py
@@ -16,19 +16,20 @@ def get_path_to_gaussian(element):
     gaussians_path = amptorch_path / "examples/GMP/valence_gaussians"
     return next(gaussians_path.glob(f"{element}_*"))
 
-def get_all_elements(*trajs):
-    return list({atom.symbol for traj in trajs for image in traj for atom in image})
+def get_all_elements(traj):
+    return list({atom.symbol for image in traj for atom in image})
 
 
-def construct_lmdb(data_dir, train_fname):#, test_fname):
+def construct_lmdb(data_dir, train_fname, test_fname):
     print(f"Loading {train_fname}")
     train_images = Trajectory(data_dir / train_fname)
-    # test_images = Trajectory(data_dir / test_fname)
-    lmdb_path = (data_dir / train_fname).with_suffix(".lmdb")
+    print(f"Loading {test_fname}")
+    test_images = Trajectory(data_dir / test_fname)
+
 
     sigmas = [0.02, 0.2, 0.4, 0.69, 1.1, 1.66, 2.66, 4.4]
 
-    elements = get_all_elements(train_images)#, test_images)
+    elements = get_all_elements(train_images)
     gaussians = {el: get_path_to_gaussian(el) for el in elements}
 
     MCSHs = {
@@ -53,10 +54,16 @@ def construct_lmdb(data_dir, train_fname):#, test_fname):
         fprimes=False)
 
     n_train = len(train_images)
+    n_test = len(test_images)
 
     train_feats = [
         a2d.convert(img, idx=idx)
-        for idx, img in tenumerate(train_images, desc='Calculating descriptors', total=n_train, unit=' images')
+        for idx, img in tenumerate(train_images, desc='Calculating train descriptors', total=n_train, unit=' images')
+    ]
+
+    test_feats = [
+        a2d.convert(img, idx=idx)
+        for idx, img in tenumerate(test_images, desc='Calculating test descriptors', total=n_test, unit=' images')
     ]
 
     scaling = {"type": "normalize", "range": (0, 1)}
@@ -66,7 +73,10 @@ def construct_lmdb(data_dir, train_fname):#, test_fname):
     feature_scaler.norm(train_feats)
     target_scaler.norm(train_feats)
 
-    to_save = {
+    feature_scaler.norm(test_feats)
+    target_scaler.norm(test_feats)
+
+    train_data = {
         **dict(enumerate(train_feats)),
         "feature_scaler": feature_scaler,
         "target_scaler": target_scaler,
@@ -74,7 +84,17 @@ def construct_lmdb(data_dir, train_fname):#, test_fname):
         "elements": elements,
         "descriptor_setup": descriptor_setup,
     }
-    save_to_lmdb(to_save, lmdb_path)
+    save_to_lmdb(train_data, lmdb_path=(data_dir / train_fname).with_suffix(".lmdb"))
+
+    test_data = {
+        **dict(enumerate(test_feats)),
+        "feature_scaler": feature_scaler,
+        "target_scaler": target_scaler,
+        "length": n_test,
+        "elements": elements,
+        "descriptor_setup": descriptor_setup,
+    }
+    save_to_lmdb(test_data, lmdb_path=(data_dir / test_fname).with_suffix(".lmdb"))
 
 def save_to_lmdb(to_save, lmdb_path):
     db = lmdb.open(
@@ -99,5 +119,5 @@ if __name__ == '__main__':
     construct_lmdb(
         data_dir=bdqm_hpopt_path / "data",
         train_fname="oc20_3k_train.traj",
-        # test_fname="oc20_300_test.traj",
+        test_fname="oc20_300_test.traj",
     )
