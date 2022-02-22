e230b738d1b290663474171e3def084cbef20e38
Alex McKenzie <alex.thewlis.mckenzie@gmail.com>
2022-02-22 12:37:44 -0500

    Checkpoint

---
+++ hpopt/delete_study.py
@@ -0,0 +1,15 @@
+import optuna
+
+import os
+
+def construct_connection_string():
+    username = os.getenv('MYSQL_USERNAME')
+    password = os.getenv('MYSQL_PASSWORD')
+    node = os.getenv('MYSQL_NODE')
+    db = os.getenv('DB')
+    return f"mysql+pymysql://{username}:{password}@{node}/{db}"
+
+
+if __name__ == '__main__':
+    connection_string = construct_connection_string()
+    optuna.delete_study(study_name="distributed-amptorch-tuning", storage=connection_string)
