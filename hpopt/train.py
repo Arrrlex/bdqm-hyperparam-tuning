86eddfeaf084b45192826458bc8c5c99e89c695d
Alex McKenzie <alex.thewlis.mckenzie@gmail.com>
2022-02-22 10:45:56 +0100

    Run hyperparameter optimization with optuna

--- hpopt/train.py
+++ hpopt/train.py
@@ -1,12 +1,13 @@
 import torch
 from ase.io import Trajectory
 from amptorch.trainer import AtomsTrainer
-import amptorch
 import numpy as np
-
-from pathlib import Path
+import optuna
+from torch import nn
 
 from utils import bdqm_hpopt_path
+
+gpus = int(torch.cuda.is_available())
 
 data_path = bdqm_hpopt_path / 'data'
 
@@ -23,22 +24,54 @@ y_test = np.array([img.get_potential_energy() for img in test_imgs])
 #  - Try running on GPU
 #  - Try parallelizing
 #  - Try dockerizing?
+#  - Incorporate pruning
 
-def calc_loss(**params):
-    model_params = ["num_layers", "num_nodes", "batchnorm"]
-    optim_params = ["lr", "batch_size", "loss", "metric"]
+def objective(trial):
+    num_layers = trial.suggest_int('num_layers', 3, 8)
+    num_nodes = trial.suggest_int('num_nodes', 4, 15)
+
+    # model params
+    batchnorm = False
+    dropout = False
+    dropout_rate = 0.5
+    initialization = "xavier"
+    activation = nn.Tanh
+
+    # optim params
+    lr = 1e-3
+    batch_size = 253
+    loss = "mae"
 
     config = {
-        "model": {"name": "singlenn", "get_forces": False},
-        "optim": {"epochs": 5, "force_coefficient": 0.},
-        "dataset": {"lmdb_path": [str(data_path / "train.lmdb")], "cache": "full"},
-        "cmd": {"debug": False, "seed": 12, "identifier": "test", "dtype": "torch.FloatTensor"},
+        "model": {
+            "num_layers": num_layers,
+            "num_nodes": num_nodes,
+            "name": "singlenn",
+            "get_forces": False,
+            "batchnorm": batchnorm,
+            "dropout": dropout,
+            "dropout_rate": dropout_rate,
+            "initialization": initialization,
+            "activation": activation,
+        },
+        "optim": {
+            "gpus": gpus,
+            "lr": lr,
+            "batch_size": batch_size,
+            "loss": loss,
+        },
+        "dataset": {
+            "lmdb_path": [str(data_path / "train.lmdb")],
+            "cache": "full",
+        },
+        "cmd": {
+            "debug": False,
+            "seed": 12,
+            "identifier": "test",
+            "dtype": "torch.FloatTensor",
+            "verbose": False,
+        },
     }
-
-    for k in model_params:
-        config["model"][k] = params[k]
-    for k in optim_params:
-        config["optim"][k] = params[k]
 
     trainer = AtomsTrainer(config)
     trainer.train()
@@ -47,12 +80,11 @@ def calc_loss(**params):
 
     return np.mean(np.abs(y_pred - y_test))
 
-print(calc_loss(
-    num_layers=3,
-    num_nodes=10,
-    batchnorm=True,
-    lr=1e-3,
-    batch_size=16,
-    loss="mse",
-    metric="mae",
-))
+def run_hyperparameter_optimization():
+    study = optuna.create_study()
+    study.optimize(objective, n_trials=5)
+
+    print(study.best_params)
+
+if __name__ == '__main__':
+    run_hyperparameter_optimization()
