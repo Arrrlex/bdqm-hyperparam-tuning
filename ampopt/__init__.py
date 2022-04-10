8f8639006cec04df4edb6973f4ff2a9c66ca9c21
Alex McKenzie <alex.thewlis.mckenzie@gmail.com>
2022-04-10 15:05:06 +0200

    Fix bug

--- ampopt/__init__.py
+++ ampopt/__init__.py
@@ -1,12 +1,12 @@
 from ampopt.jobs import ensure_mysql_running, run_tuning_jobs, show_running_jobs
-from ampopt.preprocess import create_lmdbs, create_validation_split
+from ampopt.preprocess import create_lmdbs, create_valid_split
 from ampopt.study import delete_studies, generate_report, view_all_studies
 from ampopt.train import tune
 
 __all__ = [
     "tune",
     "create_lmdbs",
-    "create_validation_split",
+    "create_valid_split",
     "delete_studies",
     "generate_report",
     "view_all_studies",
